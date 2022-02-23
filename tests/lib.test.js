a412e2dd7a3977cbedc81367a03c30d7b20b1cf9
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-23 21:27:05 +0100

    refactor the test

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,15 +1,17 @@
 const lib = require("../lib");
 
-test("should return a positive number if input is positive", () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
-});
+describe("absolute test", () => {
+  it("should return a positive number if input is positive", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
 
-test("should return a negative number if input is negative", () => {
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
-test("should return 0 if input is 0", () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
+  it("should return a negative number if input is negative", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+  it("should return 0 if input is 0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
