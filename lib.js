741e55b4fb28c8e32d9bc4433a4c7a6285b5dff9
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-02-24 15:56:35 +0100

    testing fizzbuzz exercise

--- lib.js
+++ lib.js
@@ -10,10 +10,21 @@ module.exports.getCurrencies = function () {
   return ["NGN", "EUR", "DIR"];
 };
 module.exports.getProducts = function (productId) {
-  return { id: productId, price: 10, quanttity: 1};
+  return { id: productId, price: 10, quanttity: 1 };
 };
-module.exports.registerUser = function(username){
-    if(!username) throw new Error('Username is required')
+module.exports.registerUser = function (username) {
+  if (!username) throw new Error("Username is required");
 
-    return({id: new Date().getTime(), username: username})
-}
+  return { id: new Date().getTime(), username: username };
+};
+module.exports.fizzBuzz = function (input) {
+  if (typeof input !== "number") throw new Error("input must be a number");
+
+  if (input % 3 === 0) return Fizz;
+
+  if (input % 5 === 0) return Buzz;
+
+  if (input % 5 === 0 && input % 3 === 0) return FizzBuzz;
+  
+  return input;
+};
